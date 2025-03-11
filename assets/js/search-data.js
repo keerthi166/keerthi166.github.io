351a8cc95c6289e95f52d401fe2c991890779cf2
keerthi166 <keerthi166@users.noreply.github.com>
2025-03-11 19:21:24 +0000

    Deploying to gh-pages from @ keerthi166/keerthi166.github.io@2c8f3c86f4366a7f078903c76bb39aa9cd7aa849 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Publications by categories in reversed chronological order. For recent publications, please visit my Google Scholar.",
+          description: "Publications by categories in reversed chronological order. For recent publications, please visit my  Google Scholar .",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
